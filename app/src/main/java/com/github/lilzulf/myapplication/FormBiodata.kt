c35c52cdc925ffd9d79eaf01e12a06010c0819ec
najib <najibzulfan@gmail.com>
2020-05-04 21:15:29 +0700

    (+) Layout Profile

--- app/src/main/java/com/github/lilzulf/myapplication/FormBiodata.kt
+++ app/src/main/java/com/github/lilzulf/myapplication/FormBiodata.kt
@@ -1,12 +1,20 @@
 package com.github.lilzulf.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_form_biodata.*
 
 class FormBiodata : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_biodata)
+
+        // Intent
+        BtSave.setOnClickListener {
+            startActivity(Intent(this@FormBiodata,ProfileActivity::class.java))
+        }
+
     }
 }
